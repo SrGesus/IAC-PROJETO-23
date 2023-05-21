c113614030b469fab3c6a4706596b17d85627228
SrGesus <108523575+SrGesus@users.noreply.github.com>
2023-05-21 18:08:59 +0100

    Added conversion from Hex to Decimal and display counter

--- grupo25.asm
+++ grupo25.asm
@@ -8,34 +8,65 @@
 ; *****************************************************************************
 ; * Constantes
 ; *****************************************************************************
-DISPLAYS  EQU 0A000H
-TEC_LIN   EQU 0C000H
-TEC_COL   EQU 0E000H
-LINHA     EQU 01111H  ; Vai rodar entre 1111H-2222H-4444H-8888H-1111H
-MASCARA   EQU 0FH
+  DISPLAYS  EQU 0A000H
+  TEC_LIN   EQU 0C000H
+  TEC_COL   EQU 0E000H
+  LINHA     EQU 01111H  ; Vai rodar entre 1111H-2222H-4444H-8888H-1111H
+  MASCARA   EQU 0FH
 
-PLACE 1000H
+; *****************************************************************************
+; * Alocação da Stack
+; * Reservados 512 bytes (100H words)
+; *****************************************************************************
+  PLACE 1000H
+pilha:
+  STACK 100H
 SP_inicial:
 
 
-PLACE 0000H
 
-  MOV SP, SP_inicial
+; *****************************************************************************
+; * Rotinas correspondentes a cada tecla
+; *****************************************************************************
+LISTA_ROTINAS:
+  WORD faz_nada ; Tecla 0
+  WORD faz_nada ; Tecla 1
+  WORD faz_nada ; Tecla 2
+  WORD incrementa_display ; Tecla 3
+  WORD faz_nada ; Tecla 4
+  WORD faz_nada ; Tecla 5
+  WORD faz_nada ; Tecla 6
+  WORD decrementa_display ; Tecla 7
+  WORD faz_nada ; Tecla 8
+  WORD faz_nada ; Tecla 9
+  WORD faz_nada ; Tecla A
+  WORD faz_nada ; Tecla B
+  WORD faz_nada ; Tecla C
+  WORD faz_nada ; Tecla D
+  WORD faz_nada ; Tecla E
+  WORD faz_nada ; Tecla F
+
+
+; *****************************************************************************
+; * Inicializações dos Registos e Stack Pointer
+; *****************************************************************************
+  PLACE 0000H
 
 inicio:
-; inicializações
+  MOV SP, SP_inicial
   MOV  R2, TEC_LIN   ; endereço do periférico das linhas
   MOV  R3, TEC_COL   ; endereço do periférico das colunas
   MOV  R4, DISPLAYS  ; endereço do periférico dos displays
   MOV  R5, MASCARA   ; para isolar os 4 bits de menor peso, ao ler as colunas do teclado
+  MOV  R6, 0000H     ; reseta o valor do display para 0
+  MOV [R4], R0  ; Escreve 0 no display
 
 ; *****************************************************************************
-; Lê o teclado em ciclo e executa 
+; Lê o teclado em ciclo e executa as rotinas para cada tecla
 ; *****************************************************************************
 ler_teclado:
   MOV R1, LINHA ; Linha toma valor inicial 1111H
-  MOV R0, 0     
-  MOV [R4], R0  ; Escreve 0 no display
+  MOV R0, 0
 
 espera_tecla:
   ROL R1, 1     ; Roda o valor da linha representado pelo least significant nibble
@@ -46,17 +77,15 @@ espera_tecla:
   JZ espera_tecla ; se nenhuma tecla for premida continuar ciclo
   
   CALL valor_teclado
-  MOV [R4], R0    
+  CALL executa_comando
 
-ha_tecla:
-  MOVB [R2], R1
+ha_tecla: ; Ciclo enquanto a tecla estiver a ser premida
+  MOVB [R2], R1 
   MOVB R0, [R3]
   AND R0, R5
   CMP R0, 0
   JNZ ha_tecla
   JMP ler_teclado
-
-
 
 ; *****************************************************************************
 ; * Rotina para converter linha e coluna para valor 0H até FH sem loops
@@ -86,4 +115,90 @@ valor_teclado:
   POP   R2    ; R2 volta a tomar valor anterior
   POP   R1    ; R1 volta a tomar valor anterior
   RET
+
+
+
+; *****************************************************************************
+; Rotina que executa o commando correspondente a cada tecla
+; R0: Valor da tecla na gama 0000H-000FH
+; *****************************************************************************
+executa_comando:
+  PUSH R1
+  PUSH R0
+  
+  MOV R1, LISTA_ROTINAS
+  SHL R0, 1
+  ADD R1, R0
+  MOV R0, [R1]
+  CALL R0
+  
+  POP R0
+  POP R1
+  RET
+
+
+; *****************************************************************************
+; * Rotina que não faz nada para quando tecla não corresponde a nenhuma ação
+; *****************************************************************************
+faz_nada:
+  RET
+
+; *****************************************************************************
+; * Incrementa o Display por um valor
+; * R4: Endereço do display
+; * R6: Valor do display
+; *****************************************************************************
+incrementa_display:
+  ADD R6, 1
+  CALL escreve_display
+  RET
+
+; *****************************************************************************
+; * Decrementa o Display por um valor
+; * R4: Endereço do display
+; * R6: Valor do display
+; *****************************************************************************
+decrementa_display:
+  SUB R6, 1
+  CALL escreve_display
+  RET
+
+; *****************************************************************************
+; * Transforma valor Hex em decimal de 0 a 999 e representa-o no display
+; * R4: Endereço do display
+; * R6: Valor do display
+; *****************************************************************************
+escreve_display:
+  PUSH R6
+  PUSH R3
+  PUSH R2
+  PUSH R1
+
+  MOV R3, 10 ; Denominador das divisões, pois queremos base 10
+  
+converte_decimal:
+  MOV R1, R6
+
+  MOD R1, R3 ; unidades em base 10
+  DIV R6, R3
+
+  MOV R2, R6
+  
+  MOD R2, R3 ; dezenas em base 10
+  DIV R6, R3 ; o resto vai para as dezenas
+  
+  SHL R6, 4 ; R6 0000
+  OR R6, R2 ; R6 R2 
+
+  SHL R6, 4 ; R6 R2 0000
+  OR R6, R1 ; R6 R2 R1
+
+  MOV [R4], R6 ; escreve valor decimal no display
+
+  POP R1
+  POP R2
+  POP R3
+  POP R6
+  RET
+
 
