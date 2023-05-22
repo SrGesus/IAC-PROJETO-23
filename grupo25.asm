58b65e8dba3ab5d8a448aeb5a49631f6159f8cad
SrGesus <108523575+SrGesus@users.noreply.github.com>
2023-05-22 18:26:52 +0100

    Added drawing for multi line objects (move object is ugly fix later)

--- grupo25.asm
+++ grupo25.asm
@@ -14,6 +14,19 @@
   LINHA     EQU 01111H  ; Vai rodar entre 1111H-2222H-4444H-8888H-1111H
   MASCARA   EQU 0FH
 
+  COMANDOS				EQU	6000H			; endereço de base dos comandos do MediaCenter
+
+  DEFINE_LINHA    		EQU COMANDOS + 0AH		; endereço do comando para definir a linha
+  DEFINE_COLUNA   		EQU COMANDOS + 0CH		; endereço do comando para definir a coluna
+  DEFINE_PIXEL    		EQU COMANDOS + 12H		; endereço do comando para escrever um pixel
+  APAGA_AVISO     		EQU COMANDOS + 40H		; endereço do comando para apagar o aviso de nenhum cenário selecionado
+  APAGA_ECRÃ	 		EQU COMANDOS + 02H		; endereço do comando para apagar todos os pixels já desenhados
+  SELECIONA_CENARIO_FUNDO  EQU COMANDOS + 42H		; endereço do comando para selecionar uma imagem de fundo
+  TOCA_SOM				EQU COMANDOS + 5AH		; endereço do comando para tocar um som
+
+
+
+
 ; *****************************************************************************
 ; * Alocação da Stack
 ; * Reservados 512 bytes (100H words)
@@ -26,26 +39,49 @@ SP_inicial:
 
 
 ; *****************************************************************************
-; * Rotinas correspondentes a cada tecla
+; * Variáveis
 ; *****************************************************************************
-LISTA_ROTINAS:
-  WORD faz_nada ; Tecla 0
-  WORD faz_nada ; Tecla 1
-  WORD faz_nada ; Tecla 2
-  WORD incrementa_display ; Tecla 3
-  WORD faz_nada ; Tecla 4
-  WORD faz_nada ; Tecla 5
-  WORD faz_nada ; Tecla 6
-  WORD decrementa_display ; Tecla 7
-  WORD faz_nada ; Tecla 8
-  WORD faz_nada ; Tecla 9
-  WORD faz_nada ; Tecla A
-  WORD faz_nada ; Tecla B
-  WORD faz_nada ; Tecla C
-  WORD faz_nada ; Tecla D
-  WORD faz_nada ; Tecla E
-  WORD faz_nada ; Tecla F
 
+    ; *************************************************************************
+    ; * Rotinas correspondentes a cada tecla
+    ; *************************************************************************
+LISTA_ROTINAS:
+  WORD faz_nada           ; Tecla 0
+  WORD faz_nada           ; Tecla 1
+  WORD faz_nada           ; Tecla 2
+  WORD incrementa_display ; Tecla 3
+  WORD faz_nada           ; Tecla 4
+  WORD move_objeto     ; Tecla 5
+  WORD faz_nada           ; Tecla 6
+  WORD decrementa_display ; Tecla 7
+  WORD faz_nada           ; Tecla 8
+  WORD faz_nada           ; Tecla 9
+  WORD faz_nada           ; Tecla A
+  WORD faz_nada           ; Tecla B
+  WORD faz_nada           ; Tecla C
+  WORD faz_nada           ; Tecla D
+  WORD faz_nada           ; Tecla E
+  WORD faz_nada           ; Tecla F
+
+
+
+    ; *************************************************************************
+    ; * Bonecos
+    ; *************************************************************************
+ASTEROIDE_BONECO:
+  WORD 5    ; Largura boneco
+  WORD 5    ;  Altura boneco
+
+  WORD 0FF00H, 0, 0FF00H, 0, 0FF00H
+  WORD 0, 0FF00H, 0, 0FF00H, 0
+  WORD 0FF00H, 0, 0, 0, 0FF00H
+  WORD 0, 0FF00H, 0, 0FF00H, 0
+  WORD 0FF00H, 0, 0FF00H, 0, 0FF00H
+
+ASTEROID_0:
+  WORD 0000H ; Posição : Primeiro byte é linha, segundo coluna
+  WORD 0101H ; Direção do movimento
+  WORD ASTEROIDE_BONECO ; Boneco
 
 ; *****************************************************************************
 ; * Inicializações dos Registos e Stack Pointer
@@ -54,6 +90,13 @@ LISTA_ROTINAS:
 
 inicio:
   MOV SP, SP_inicial
+
+
+  MOV  [APAGA_AVISO], R1	; apaga o aviso de nenhum cenário selecionado (o valor de R1 não é relevante)
+  MOV  [APAGA_ECRÃ], R1	; apaga todos os pixels já desenhados (o valor de R1 não é relevante)
+	MOV	R1, 0			; cenário de fundo número 0
+  MOV  [SELECIONA_CENARIO_FUNDO], R1	; seleciona o cenário de fundo
+
   MOV  R2, TEC_LIN   ; endereço do periférico das linhas
   MOV  R3, TEC_COL   ; endereço do periférico das colunas
   MOV  R4, DISPLAYS  ; endereço do periférico dos displays
@@ -88,9 +131,11 @@ ha_tecla: ; Ciclo enquanto a tecla estiver a ser premida
   JMP ler_teclado
 
 ; *****************************************************************************
-; * Rotina para converter linha e coluna para valor 0H até FH sem loops
-; * R0: Valor da coluna e Registo de retorno do valor
-; * R1: Valor da linha
+; * VALOR_TECLADO - Rotina para converter linha e coluna para valor 
+; *   0H até FH sem loops
+; * Argumentos:
+; *   R0: Valor da coluna e Registo de retorno do valor
+; *   R1: Valor da linha
 ; *****************************************************************************
 valor_teclado:
   PUSH  R1    ; guarda o valor do R1
@@ -119,12 +164,12 @@ valor_teclado:
 
 
 ; *****************************************************************************
-; Rotina que executa o commando correspondente a cada tecla
-; R0: Valor da tecla na gama 0000H-000FH
+; * EXECUTA_COMANDO - Rotina que executa o commando correspondente a cada tecla
+; * Argumentos: 
+; *   R0: Valor da tecla na gama 0000H-000FH (Valor é consumido)
 ; *****************************************************************************
 executa_comando:
   PUSH R1
-  PUSH R0
   
   MOV R1, LISTA_ROTINAS
   SHL R0, 1
@@ -132,7 +177,6 @@ executa_comando:
   MOV R0, [R1]
   CALL R0
   
-  POP R0
   POP R1
   RET
 
@@ -144,48 +188,57 @@ faz_nada:
   RET
 
 ; *****************************************************************************
-; * Incrementa o Display por um valor
-; * R4: Endereço do display
-; * R6: Valor do display
+; * INCREMENTA_DISPLAY - Incrementa o Display por um valor
+; * Argumentos:
+; *   R0: Valor é consumido
+; *   R1: Valor é consumido
+; *   R4: Endereço do display
+; *   R6: Valor do display
 ; *****************************************************************************
 incrementa_display:
   ADD R6, 1
+  MOV R0, 10
   CALL escreve_display
   RET
 
 ; *****************************************************************************
-; * Decrementa o Display por um valor
-; * R4: Endereço do display
-; * R6: Valor do display
+; * DECREMENTA_DISPLAY - Decrementa o Display por um valor
+; * Argumentos:
+; *   R0: Valor é consumido
+; *   R1: Valor é consumido
+; *   R4: Endereço do display
+; *   R6: Valor do display
 ; *****************************************************************************
 decrementa_display:
   SUB R6, 1
+  MOV R0, 10
   CALL escreve_display
   RET
 
 ; *****************************************************************************
-; * Transforma valor Hex em decimal de 0 a 999 e representa-o no display
-; * R4: Endereço do display
-; * R6: Valor do display
+; * ESCREVE_DISPLAY - Transforma valor Hex noutra base (de 1 a 16), num limite 
+; *   de 3 dígitos para base 10 é válido de 0 a 999 (0H a 3E7H) 
+; *   e representa-o no display.
+; * Argumentos:
+; *   R0: Base (Denominador das divisões)
+; *   R1: Valor é consumido
+; *   R4: Endereço do display
+; *   R6: Valor do display
 ; *****************************************************************************
 escreve_display:
   PUSH R6
-  PUSH R3
   PUSH R2
-  PUSH R1
 
-  MOV R3, 10 ; Denominador das divisões, pois queremos base 10
-  
 converte_decimal:
   MOV R1, R6
 
-  MOD R1, R3 ; unidades em base 10
-  DIV R6, R3
+  MOD R1, R0 ; unidades em base 10
+  DIV R6, R0
 
   MOV R2, R6
   
-  MOD R2, R3 ; dezenas em base 10
-  DIV R6, R3 ; o resto vai para as dezenas
+  MOD R2, R0 ; dezenas em base 10
+  DIV R6, R0 ; o resto vai para as dezenas
   
   SHL R6, 4 ; R6 0000
   OR R6, R2 ; R6 R2 
@@ -195,10 +248,163 @@ converte_decimal:
 
   MOV [R4], R6 ; escreve valor decimal no display
 
-  POP R1
   POP R2
-  POP R3
   POP R6
   RET
+
+
+; *****************************************************************************
+; * MOVE_OBJETO - Move, apaga, e desenha um objeto representado 
+; *   por uma determinada tabela.
+; * Argumentos:
+; *   R3 - Objeto
+; *****************************************************************************
+move_objeto:
+  PUSH R0
+  PUSH R1
+  PUSH R3
+  PUSH R4
+
+  MOV R3, ASTEROID_0
+
+  MOV R0, [R3]
+  MOV R1, [R3]
+  SHR R0, 8
+  SHL R1, 8
+  SHR R1, 8
+  
+  ADD R3, 4
+
+  MOV R4, [R3]
+
+  CALL apaga_boneco
+ 
+  SUB R3, 4
+
+  MOV R0, [R3]
+  ADD R3, 2
+  
+  MOV R1, [R3]
+  ADD R0, R1
+  SUB R3, 2
+  MOV [R3], R0
+  MOV R1, R0
+  SHR R0, 8
+  SHL R1, 8
+  SHR R1, 8
+
+  ADD R3, 4
+  
+ 
+
+  CALL desenha_boneco
+
+  POP R4
+  POP R3
+  POP R1
+  POP R0
+  RET
+
+; *****************************************************************************
+; * DESENHA_BONECO - Desenha um boneco na sua linha e coluna
+;	*   com a forma e cor definidas na tabela.
+; * Argumentos:
+; *   R0 - Linha (Valor é consumido)
+; *   R1 - Coluna (Valor é consumido)
+; *   R4 - Tabela que define o boneco
+; *****************************************************************************
+desenha_boneco:
+	PUSH	R2
+	PUSH	R4
+	PUSH	R5
+  PUSH  R6
+  PUSH  R7
+  
+
+  MOV R6, [R4]  ; guarda a largura do boneco
+	ADD	R4, 2			; endereço da altura do boneco (2 porque a largura é uma word)
+	MOV R7, [R4]  ; obtem a altura do boneco
+  ADD	R4, 2			; endereço da cor do 1º pixel (2 porque a largura é uma word)
+  ADD R1, R6
+proxima_linha:
+	MOV	R5, R6    ; obtém a largura do boneco
+  SUB R1, R5    ; reseta a coluna 
+desenha_linha:  ; desenha uma linha de pixels do boneco a partir da tabela
+	MOV	R2, [R4]	; obtém a cor do próximo pixel do boneco
+	CALL	escreve_pixel		; escreve cada pixel do boneco
+	ADD	R4, 2		  ; endereço da cor do próximo pixel (2 porque cada cor de pixel é uma word)
+  ADD  R1, 1    ; próxima coluna
+  SUB  R5, 1  	; menos uma coluna para tratar
+  JNZ  desenha_linha      ; continua até percorrer toda a largura da linha
+  ADD R0, 1     ; próxima linha
+  SUB R7, 1     ; menos uma linha para tratar	
+  JNZ proxima_linha
+
+  POP R7
+  POP R6
+  POP R5
+  POP R4
+  POP R2
+  RET
+
+; *****************************************************************************
+; * APAGA_BONECO - Desenha um boneco na sua linha e coluna
+;	*   com a forma e cor definidas na tabela.
+; * Argumentos:
+; *   R0 - Linha
+; *   R1 - Coluna
+; *   R4 - Tabela que define o boneco
+; *****************************************************************************
+apaga_boneco:
+	PUSH	R2
+	PUSH	R4
+	PUSH	R5
+  PUSH  R6
+  PUSH  R7
+  
+
+  MOV R6, [R4]  ; guarda a largura do boneco
+	ADD	R4, 2			; endereço da altura do boneco (2 porque a largura é uma word)
+	MOV R7, [R4]  ; obtem a altura do boneco
+  ADD	R4, 2			; endereço da cor do 1º pixel (2 porque a largura é uma word)
+  ADD R1, R6
+apaga_proxima_linha:
+	MOV	R5, R6    ; obtém a largura do boneco
+  SUB R1, R5    ; reseta a coluna 
+apaga_linha:  ; desenha uma linha de pixels do boneco a partir da tabela
+	MOV	R2, 0	; obtém a cor do próximo pixel do boneco
+	CALL	escreve_pixel		; escreve cada pixel do boneco
+	ADD	R4, 2		  ; endereço da cor do próximo pixel (2 porque cada cor de pixel é uma word)
+  ADD  R1, 1    ; próxima coluna
+  SUB  R5, 1  	; menos uma coluna para tratar
+  JNZ  apaga_linha      ; continua até percorrer toda a largura da linha
+  ADD R0, 1     ; próxima linha
+  SUB R7, 1     ; menos uma linha para tratar	
+  JNZ apaga_proxima_linha
+
+  POP R7
+  POP R6
+  POP R5
+  POP R4
+  POP R2
+  RET
+
+
+
+; *****************************************************************************
+; * ESCREVE_PIXEL - Escreve pixel na linha e coluna dos argumentos 
+; * Argumentos:
+; *   R0 - linha
+; *   R1 - coluna
+; *   R2 - Cor do Pixel
+; *****************************************************************************
+escreve_pixel:
+	MOV  [DEFINE_LINHA], R0		; seleciona a linha
+	MOV  [DEFINE_COLUNA], R1		; seleciona a coluna
+	MOV  [DEFINE_PIXEL], R2		; altera a cor do pixel na linha e coluna já selecionadas
+	RET
+
+
+
 
 
